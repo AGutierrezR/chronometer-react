ef9eb41ce303f07caf86c0fd6ff775309f5662b7
Andres Gutierrez <agutierrez.codes@gmail.com>
2020-05-14 22:56:49 +0200

    Updates

--- precache-manifest.a0d61b64cabc72f15aa5cce0ce097a1f.js
+++ precache-manifest.a0d61b64cabc72f15aa5cce0ce097a1f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9b4c898299287b7c8d74e84e6a99f49",
+    "revision": "dd0c87f168711519052dead2f05ec2af",
     "url": "/chronometer-react/index.html"
   },
   {
-    "revision": "210b392e8dfa19d30f4e",
+    "revision": "b591b5c842d8fdaadc82",
     "url": "/chronometer-react/static/css/main.1257e240.chunk.css"
   },
   {
-    "revision": "d2b072c8ce66fdb3f437",
-    "url": "/chronometer-react/static/js/2.8b07fb96.chunk.js"
+    "revision": "bb02976d884a33b84f5a",
+    "url": "/chronometer-react/static/js/2.38654ed7.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/chronometer-react/static/js/2.8b07fb96.chunk.js.LICENSE.txt"
+    "url": "/chronometer-react/static/js/2.38654ed7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "210b392e8dfa19d30f4e",
-    "url": "/chronometer-react/static/js/main.c117e2c6.chunk.js"
+    "revision": "b591b5c842d8fdaadc82",
+    "url": "/chronometer-react/static/js/main.70896fbb.chunk.js"
   },
   {
     "revision": "8ca708def3c2355a9fb9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chronometer-react/precache-manifest.583b932b60ffb545830d7f701cafdf20.js"
+  "/chronometer-react/precache-manifest.a0d61b64cabc72f15aa5cce0ce097a1f.js"
 );
 
 self.addEventListener('message', (event) => {
