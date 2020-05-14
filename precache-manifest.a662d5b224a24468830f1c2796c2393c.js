1d85e0a759cdf135fc04c059dcab4a59964b24be
Andres Gutierrez <agutierrez.codes@gmail.com>
2020-05-14 21:42:14 +0200

    Updates

---
+++ precache-manifest.a662d5b224a24468830f1c2796c2393c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e64998e5431b7ae894581386ad0cbc1",
+    "url": "/chronometer-react/index.html"
+  },
+  {
+    "revision": "326ff4b0a976908d8dc3",
+    "url": "/chronometer-react/static/css/main.1257e240.chunk.css"
+  },
+  {
+    "revision": "d2b072c8ce66fdb3f437",
+    "url": "/chronometer-react/static/js/2.8b07fb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chronometer-react/static/js/2.8b07fb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326ff4b0a976908d8dc3",
+    "url": "/chronometer-react/static/js/main.da14b119.chunk.js"
+  },
+  {
+    "revision": "8ca708def3c2355a9fb9",
+    "url": "/chronometer-react/static/js/runtime-main.1ae2b913.js"
+  }
+]);
